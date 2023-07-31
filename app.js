b54d54b6c65eb8f52ba62aa6684167187c882407
enkai67 <nkai940372@gmail.com>
2023-08-01 01:20:12 +0800

    fix edit function

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 const tasks = require('./routes/tasks')
 
-
 //middleware
-
+app.use(express.static('./public'))
 app.use(express.json())
+app.use(errorHandlerMiddleware)
 
 //routes
-app.get('/hello', (req, res)=>{
-  res.send('Task Manager App')
-})
 
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
 
 
 //app.get('api/v1/tasks')
 //app.post('api/v1/tasks')
-//app.get('api/v1/tasks:id')
-//app.patch('api/v1/tasks:id')
-//app.delete('api/v1/tasks:id')
+//app.get('api/v1/tasks/:id')
+//app.patch('api/v1/tasks/:id')
+//app.delete('api/v1/tasks/:id')
 
 const start = async () => {
   try {
