99faaf34f1d077e1e6223d7a65ee0d082cf5641e
enkai67 <nkai940372@gmail.com>
2023-07-31 18:19:16 +0800

    first build

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+const tasks = require('./routes/tasks')
+
+
+//middleware
+
+app.use(express.json())
+
+//routes
+app.get('/hello', (req, res)=>{
+  res.send('Task Manager App')
+})
+
+app.use('/api/v1/tasks', tasks)
+
+
+//app.get('api/v1/tasks')
+//app.post('api/v1/tasks')
+//app.get('api/v1/tasks:id')
+//app.patch('api/v1/tasks:id')
+//app.delete('api/v1/tasks:id')
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, ()=>{
+    console.log(`server is listening on port ${port}...`)})
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
